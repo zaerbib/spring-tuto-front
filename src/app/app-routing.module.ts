7acb2544452bb2a7e08632925d5b0e074a2ca5d0
zaerbib <ilunga.joel@yahoo.fr>
2019-10-08 22:21:05 +0200

    Rien

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CustomersComponent } from './customers/customers.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { OfficesComponent } from './offices/offices.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'customers',
+    component: CustomersComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'employees',
+    component: EmployeesComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'offices',
+    component: OfficesComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
+import { HomeComponent } from './home/home.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { OfficesComponent } from './offices/offices.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersComponent,
+    HomeComponent,
+    EmployeesComponent,
+    OfficesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
